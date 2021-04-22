41ab63ddd52196ec9eb4b2173dc6554457d58ace
BNNProduction <uthai.boonyaba@gmail.com>
2021-04-22 12:30:11 +0700

    login first

--- my-dream-app/src/app/app-routing.module.ts
+++ my-dream-app/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-component', component: AppComponent },
+  { path: 'login-component', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
