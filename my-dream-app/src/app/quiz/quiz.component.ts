517ae2a551fd2716106c2325895383fed0aae42d
BNNProduction <uthai.boonyaba@gmail.com>
2021-04-22 17:08:41 +0700

    release quiz

--- my-dream-app/src/app/quiz/quiz.component.ts
+++ my-dream-app/src/app/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+
+  name: String = "";
+
+  likeText: String;
+  like:number =100;
+  disLikeText: String;
+  dislike:number =25;
+  btnStyle: String;
+  btnStyle2: String;
+  constructor() {
+
+    this.likeText =  "Like | "+this.like;
+    this.disLikeText =  "Dislike | "+this.dislike;
+    this.btnStyle = "like-button-deactive";
+    this.btnStyle2 = "like-button-deactive";
+   }
+  onLikeClick(){
+    if(this.btnStyle === "like-button-active"){
+       this.btnStyle = "like-button-deactive";
+       this.like--;
+    }else{
+      this.btnStyle = "like-button-active";
+      this.like++;
+    }
+    this.likeText = "Like | "+this.like;
+    //console.log(this.name);
+  }
+  onDisLikeClick(){
+    if(this.btnStyle2 === "like-button-active"){
+      this.btnStyle2 = "like-button-deactive";
+      this.dislike--;
+      
+   }else{
+     this.btnStyle2 = "like-button-active";
+     this.dislike++;
+   }
+   this.disLikeText =  "Dislike | "+this.dislike;
+    //console.log(this.name);
+  }
 
   ngOnInit(): void {
+    //console.log("Init : "+this.name);
   }
 
 }
