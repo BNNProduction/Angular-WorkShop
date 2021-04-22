7204c34829340c31e024e25c6e6e8e63b85895a4
BNNProduction <uthai.boonyaba@gmail.com>
2021-04-22 15:10:08 +0700

    add star Rating

--- my-dream-app/src/app/login/login.component.ts
+++ my-dream-app/src/app/login/login.component.ts
@@ -18,6 +18,7 @@ export class LoginComponent implements OnInit {
       new User(1, 'user 01', 'last 01', 'xxx@demo1.com',1),
       new User(2, 'user 02', 'last 02', 'xxx@demo2.com',2),
       new User(3, 'user 03', 'last 03', 'xxx@demo3.com',3),
+      new User(4, 'user 04', 'last 04', 'xxx@demo3.com',4.2),
     ];
   }
 
--- my-dream-app/src/app/rating/rating.component.ts
+++ my-dream-app/src/app/rating/rating.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.css']
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements OnChanges,OnInit {
 
   @Input()
   targetScore: number = 0;
 
+  @Input()
+  isStar: boolean = false;
+  starWidth: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.starWidth = this.targetScore * 75 / 5;
   }
+
+  ngOnChanges(): void { 
+  
+    }
+
 
 }
